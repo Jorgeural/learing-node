2bd1cec19f7fa11a7f9a09d19582a68e6a0e3be1
Jorge <jorgeural88@gmail.com>
2017-05-16 17:00:11 +0000

    finished timestamp

--- FCC/timestamp-microservice/app.js
+++ FCC/timestamp-microservice/app.js
@@ -1,18 +1,26 @@
-const http = require("http");
-const fs = require("fs");
-
+//Load required modules
+const express = require("express");
+const bodyParser = require("body-parser");
+//load our dates.js module
 const dates = require("./dates.js");
+//Create new express instance and configurations
+const app = express();
+app.use(bodyParser.json());
 
-function handler (req, res){
-    if(req.url === "/" || req.url === "/home"){
-        res.writeHead(200, {"Content-Type" : "text/html"}); 
-        var homeHtml = fs.createReadStream(__dirname + "/home.html", "utf8");
-        homeHtml.pipe(res);
-    } else {
-        res.end(dates.formatDate(req.url)); 
-    }
-}
 
-const server = http.createServer(handler);
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/home.html");
+});
 
-server.listen(8080, "0.0.0.0");
+app.get("/:date", function(req, res){
+    //Variable to store the request param
+    var date = req.params.date;
+    //use our module funciton
+    var formated = dates.formatDate(date);
+    //send info 
+    res.json(formated);
+});
+
+
+//Set wich port to listen
+app.listen(8080);
--- FCC/timestamp-microservice/dates.js
+++ FCC/timestamp-microservice/dates.js
@@ -1,10 +1,43 @@
-const fs = require("fs");
+//Create a new module
 
-var formatDate = (date) => {
-    console.log(typeof date);
-    return date;
+// Implement logic here
+
+var dateOptions = {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
 };
 
+var dateObj = {
+    unix: null,
+    natural: null
+}
+
+
+var formatDate = (date) => {
+    
+    var onlyNumbers = !/[^\d]/g.test(date)
+    
+    
+    if(onlyNumbers){
+        dateObj.unix = Number(date);
+        var naturalDate = new Date(dateObj.unix * 1000);
+        dateObj.natural = naturalDate.toLocaleDateString('en-US', dateOptions);
+    } else {
+        var naturalDate = new Date(date) || null;
+        console.log(naturalDate);
+        dateObj.natural = naturalDate.toLocaleDateString("en-US", dateOptions);
+        var unixDate = naturalDate.getTime()/1000;
+        console.log(unixDate);
+        dateObj.unix = unixDate;
+        
+    }
+    
+    
+    return dateObj;
+};
+
+//Export the module to be usable in app.js
 module.exports = {
     formatDate
 };
